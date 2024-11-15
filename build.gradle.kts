1a0406a4653ba01c25a1ef4b01a73fa8b719658b
ubtbu1 <lailixun1@gmail.com>
2024-11-15 11:58:12 +0800

    upgrade AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 
 plugins {
     // 应用插件时，使用 id() 函数，并指定版本
-    id("com.android.application") version "8.6.1" apply false
+    id("com.android.application") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.21" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.8.3" apply false
 }
