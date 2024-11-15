ea0f8645ed82b50f03234dc6074c9b975e40a6f3
lailixun <lailixun1@gmail.com>
2024-11-15 14:17:13 +0800

    Revert "Revert "Add chen code""

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+// 项目级别的 build.gradle.kts 文件
+
+plugins {
+    // 应用插件时，使用 id() 函数，并指定版本
+    id("com.android.application") version "8.7.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.8.3" apply false
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
